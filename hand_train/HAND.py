9ee5f3feeca85b518548a2b3541e70b920aa0bb4
xcWhy <elena@dilian.net>
2022-03-27 13:37:27 +0300

    changes

--- hand_train/HAND.py
+++ hand_train/HAND.py
@@ -8,10 +8,16 @@ cap = cv2.VideoCapture(1)
 cap.set(3, wCam)
 cap.set(4, hCam)
 
-folderPath = "imgs_fingers"
+folderPath = "../imgs_fingers"
 myList = os.listdir(folderPath) #its important to be in order
 
 while True:
     success, img = cap.read()
+    print(success)
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+
+    if cv2.waitKey(1) == ord('q'):  # if we press q it will stop and it will break
+        break
+
+cap.release()
+cv2.destroyAllWindows()
