49f1465b6521f9d5fdcf124fbf75958530ceb629
xcWhy <elena@dilian.net>
2022-03-29 10:57:41 +0300

    VDIGNAT PRYST

--- hand_train/test2.py
+++ hand_train/test2.py
@@ -9,44 +9,46 @@ mp_draw = mp.solutions.drawing_utils
 
 hands = mp_hands.Hands()
 
+
+#HandLandmark.
+
+def hand_sign(handMark):
+    i = 0
+    #print("pok----", handMark['HandLandmark.INDEX_FINGER_TIP']) #STOInostite ne se promenqt
+    #print("sre----", handMark['HandLandmark.MIDDLE_FINGER_TIP'])
+    if handMark['HandLandmark.INDEX_FINGER_TIP'] < handMark['HandLandmark.MIDDLE_FINGER_TIP']:
+        print("YES - PRYST NAGORE------------------------------------------")
+        i += 1
+        print(i)
+
 all_hand_landmarks = {
-    "HandLandmark.WRIST": 0,
-    "HandLandmark.THUMB_CMC": 0,
-    "HandLandmark.THUMB_MCP": 0,
-    "HandLandmark.THUMB_IP": 0,
-    "HandLandmark.THUMB_TIP": 0,
-    "HandLandmark.INDEX_FINGER_MCP": 0,
-    "HandLandmark.INDEX_FINGER_PIP": 0,
-    "HandLandmark.INDEX_FINGER_DIP": 0,
-    "HandLandmark.INDEX_FINGER_TIP": 0,
-    "HandLandmark.MIDDLE_FINGER_MCP": 0,
-    "HandLandmark.MIDDLE_FINGER_PIP": 0,
-    "HandLandmark.MIDDLE_FINGER_DIP": 0,
-    "HandLandmark.MIDDLE_FINGER_TIP": 0,
-    "HandLandmark.RING_FINGER_MCP": 0,
-    "HandLandmark.RING_FINGER_PIP": 0,
-    "HandLandmark.RING_FINGER_DIP": 0,
-    "HandLandmark.RING_FINGER_TIP": 0,
-    "HandLandmark.PINKY_MCP": 0,
-    "HandLandmark.PINKY_PIP": 0,
-    "HandLandmark.PINKY_DIP": 0,
-    "HandLandmark.PINKY_TIP": 0
+    "HandLandmark.WRIST": 0, #0
+    "HandLandmark.THUMB_CMC": 0, #1
+    "HandLandmark.THUMB_MCP": 0, #2
+    "HandLandmark.THUMB_IP": 0, #3
+    "HandLandmark.THUMB_TIP": 0, #4
+    "HandLandmark.INDEX_FINGER_MCP": 0, #5
+    "HandLandmark.INDEX_FINGER_PIP": 0, #6
+    "HandLandmark.INDEX_FINGER_DIP": 0, #7
+    "HandLandmark.INDEX_FINGER_TIP": 0, #8
+    "HandLandmark.MIDDLE_FINGER_MCP": 0, #9
+    "HandLandmark.MIDDLE_FINGER_PIP": 0, #10
+    "HandLandmark.MIDDLE_FINGER_DIP": 0, #11
+    "HandLandmark.MIDDLE_FINGER_TIP": 0, #12
+    "HandLandmark.RING_FINGER_MCP": 0, #13
+    "HandLandmark.RING_FINGER_PIP": 0, #14
+    "HandLandmark.RING_FINGER_DIP": 0, #15
+    "HandLandmark.RING_FINGER_TIP": 0, #16
+    "HandLandmark.PINKY_MCP": 0, #17
+    "HandLandmark.PINKY_PIP": 0, #18
+    "HandLandmark.PINKY_DIP": 0, #19
+    "HandLandmark.PINKY_TIP": 0 #20
 }
-
-def hand_sign(coords, landmarks_list, point):
-    #print(landmarks_list)
-    #print(coords)
-    sorted(coords)
-    x, y = coords
-    #print(y)
-
-    #if point == landmarks_list[8]:
 
 
 while True:
     success, img = cap.read()
     #print(success)
-    landmarks_list = []
 
     with mp_hands.Hands(static_image_mode=True, max_num_hands=4) as hands:
         color = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -63,22 +65,22 @@ while True:
             for point in mp_hands.HandLandmark:
 
                 normalizedLandmark = handLandmarks.landmark[point]
-                print(normalizedLandmark)
+                #print(normalizedLandmark)
 
                 normCoords = normalizedLandmark.x, normalizedLandmark.y
                 pixelCoords = tuple(round(coord * dimension) for coord, dimension in zip(normCoords, screen))
 
-                print(pixelCoords)
-
-                print(str(point))
-
                 x, y = sorted(pixelCoords)
 
-                if str(point) in all_hand_landmarks:
-                    all_hand_landmarks[point] = y
-                    print(all_hand_landmarks)
+                #print(point)
 
-                #hand_sign(, landmarks_list, point)
+                if str(point) in all_hand_landmarks:
+                    #print("HEY U HERE?")
+                    all_hand_landmarks[str(point)] = y
+                    #print(all_hand_landmarks)
+                    #print(all_hand_landmarks[point])
+
+                hand_sign(all_hand_landmarks)
                 #print(all_hand_landmarks)
 
 
