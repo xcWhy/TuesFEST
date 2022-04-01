da9cb5dfd4dc979682ae2918b2db69ba298f80fe
xcWhy <elena@dilian.net>
2022-04-01 14:50:34 +0300

    arrange_code

--- hand_train/Hand_making.py
+++ hand_train/Hand_making.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import time
+import os
 
 cap = cv2.VideoCapture(0)
 
@@ -9,6 +10,9 @@ mp_draw = mp.solutions.drawing_utils
 
 hands = mp_hands.Hands()
 
+folderPath = "../imgs_fingers"
+myList = os.listdir(folderPath)
+
 
 #HandLandmark.
 
@@ -16,6 +20,18 @@ def hand_sign(handMark, thumb, handsType):
     num_Hands = []
 
     for i in range(0, len(handsType)):
+
+        if handsType[i] == 'Right':
+
+            if thumb['HandLandmark.THUMB_TIP'] > thumb['HandLandmark.THUMB_IP']:
+                num_Hands.append(1)
+                print("thumbbb right")
+
+        if handsType[i] == 'Left':
+
+            if thumb['HandLandmark.THUMB_TIP'] < thumb['HandLandmark.THUMB_IP']:
+                num_Hands.append(1)
+                print("thumbbb lefttt")
 
         if handMark['HandLandmark.INDEX_FINGER_TIP'] < handMark['HandLandmark.INDEX_FINGER_PIP']:
             num_Hands.append(1)
@@ -29,20 +45,12 @@ def hand_sign(handMark, thumb, handsType):
         if handMark['HandLandmark.PINKY_TIP'] < handMark['HandLandmark.PINKY_PIP']:
             num_Hands.append(1)
 
-        if handsType[i] == 'Right':
-
-            if thumb['HandLandmark.THUMB_TIP'] > thumb['HandLandmark.THUMB_IP']:
-                num_Hands.append(1)
-
-        if handsType[i] == 'Left':
-
-            if thumb['HandLandmark.THUMB_TIP'] < thumb['HandLandmark.THUMB_IP']:
-                num_Hands.append(1)
-
     #print(num_Hands)
 
     num = num_Hands.count(1)
-    print(num)
+
+    cv2.putText(img, f'Fingers: {int(num)}', (250, 50), cv2.FONT_ITALIC, 1, 255, 1)
+    #print(num)
 
 
 thumb = {
@@ -73,81 +81,77 @@ long_fingers = {
 }
 
 
-while True:
-    success, img = cap.read()
-    #print(success)
+with mp_hands.Hands(static_image_mode=True, max_num_hands=6) as hands:
+    while True:
+        success, img = cap.read()
+        # print(success)
+        img = cv2.flip(img, 1)
 
-
-    with mp_hands.Hands(static_image_mode=True, max_num_hands=6) as hands:
         color = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         results = hands.process(color)
         height, width, channel = img.shape
         screen = height, width
 
 
-    if results.multi_hand_landmarks:
-        #print(results.multi_hand_landmarks)
-        handsType = []
+        if results.multi_hand_landmarks:
+            #print(results.multi_hand_landmarks)
+            handsType = []
 
-        for hand in results.multi_handedness:
-            #print(results.multi_handedness)
-            # print(hand)
-            # print(hand.classification)
-            # print(hand.classification[0])
-            handType = hand.classification[0].label
-            handsType.append(handType)
+            for hand_landmarks in results.multi_hand_landmarks:
+                for lm in hand_landmarks.landmark:
+                    height, width, channel = img.shape
+                    cx, cy = int(lm.x * width), int(lm.y * height)
+                mp_draw.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
-        #print(handsType)
+            for hand in results.multi_handedness:
+                #print(results.multi_handedness)
+                # print(hand)
+                # print(hand.classification)
+                # print(hand.classification[0])
+                handType = hand.classification[0].label
+                handsType.append(handType)
 
-        for i in range(0, len(handsType)):
-            if handsType[i] == 'Left':
-                handsType[i] = 'Right'
-                continue
+            #print(handsType)
 
-            if handsType[i] == 'Right':
-                handsType[i] = 'Left'
-                continue
+            for i in range(0, len(handsType)):
+                if handsType[i] == 'Left':
+                    handsType[i] = 'Right'
+                    continue
 
-        #print("-----", handsType)
+                if handsType[i] == 'Right':
+                    handsType[i] = 'Left'
+                    continue
 
-        for handLandmarks in results.multi_hand_landmarks:
-            for point in mp_hands.HandLandmark:
+            #print("-----", handsType)
 
-                normalizedLandmark = handLandmarks.landmark[point]
-                #print(normalizedLandmark)
+            for handLandmarks in results.multi_hand_landmarks:
+                for point in mp_hands.HandLandmark:
 
-                normCoords = normalizedLandmark.x, normalizedLandmark.y
-                pixelCoords = tuple(round(coord * dimension) for coord, dimension in zip(normCoords, screen))
+                    normalizedLandmark = handLandmarks.landmark[point]
+                    #print(normalizedLandmark)
 
-                x, y = sorted(pixelCoords)
+                    normCoords = normalizedLandmark.x, normalizedLandmark.y
+                    pixelCoords = tuple(round(coord * dimension) for coord, dimension in zip(normCoords, screen))
 
-                #print(point)
+                    x, y = sorted(pixelCoords)
 
-                if str(point) in long_fingers:
-                    #print("HEY U HERE?")
-                    long_fingers[str(point)] = y
+                    #print(point)
+
+                    if str(point) in long_fingers:
+                        #print("HEY U HERE?")
+                        long_fingers[str(point)] = y
+                        #print(all_hand_landmarks)
+                        #print(all_hand_landmarks[point])
+
+                    if str(point) in thumb:
+                        thumb[str(point)] = x
+
+                    hand_sign(long_fingers, thumb, handsType)
                     #print(all_hand_landmarks)
-                    #print(all_hand_landmarks[point])
 
-                if str(point) in thumb:
-                    thumb[str(point)] = x
-
-                hand_sign(long_fingers, thumb, handsType)
-                #print(all_hand_landmarks)
-
-
-
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            for lm in hand_landmarks.landmark:
-                height, width, channel = img.shape
-                cx, cy = int(lm.x * width), int(lm.y * height)
-            mp_draw.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-
-    img = cv2.flip(img, 1)
-    cv2.imshow("Camera", img)
-    if cv2.waitKey(1) == ord('q'):
-        break
+        cv2.imshow("Camera", img)
+        if cv2.waitKey(1) == ord('q'):
+            break
 
 cap.release()
 cv2.destroyAllWindows()
